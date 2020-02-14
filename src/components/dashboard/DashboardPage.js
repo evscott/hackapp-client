540646585233d5b6669c8f727123dfc044caea6e
Graeme Zinck <graeme.zinck@gmail.com>
2020-02-14 09:19:46 -0400

    Stylized the hackathon cards and added code to automatically populate the dashboard

--- src/components/dashboard/DashboardPage.js
+++ src/components/dashboard/DashboardPage.js
@@ -4,42 +4,49 @@ import { makeStyles } from "@material-ui/core/styles";
 import SettingsIcon from "@material-ui/icons/Settings";
 import HackathonCard from "./HackathonCard";
 
-const useStyles = makeStyles(theme => {
-  return {
+const hackathons = [
+  {
+    title: "MtA Hacks 2020",
+    startDate: "6:00PM Feb 8",
+    endDate: "1:30PM Feb 9"
+  },
+  {
+    title: "MtA Hacks 2021",
+    startDate: "6:00PM Feb 8",
+    endDate: "1:30PM Feb 9"
+  },
+  {
+    title: "MtA Hacks 2022",
+    startDate: "6:00PM Feb 8",
+    endDate: "1:30PM Feb 9"
+  }
+];
 
-  };
+const useStyles = makeStyles(theme => {
+  return {};
 });
 
 const drawerPrimary = [
-    {
-      icon: <SettingsIcon />,
-      text: "Settings"
-    }
-  ];
+  {
+    icon: <SettingsIcon />,
+    text: "Settings"
+  }
+];
+
 const drawerSecondary = [];
 
 export default function DashboardPage() {
   const classes = useStyles();
 
   return (
-    <Page title="Dashboard" drawerPrimary={drawerPrimary} drawerSecondary={drawerSecondary}>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-      <HackathonCard/>
-
+    <Page
+      title="Dashboard"
+      drawerPrimary={drawerPrimary}
+      drawerSecondary={drawerSecondary}
+    >
+      {hackathons.map(hackathon => (
+        <HackathonCard {...hackathon} />
+      ))}
     </Page>
   );
 }
--- src/components/dashboard/HackathonCard.js
+++ src/components/dashboard/HackathonCard.js
@@ -3,13 +3,66 @@ import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import { makeStyles } from "@material-ui/core/styles";
+
+import hackathonImg from "../../img/hackathon-default.jpg";
+import { largeDrawerWidth } from "../page/Drawer";
 
 const useStyles = makeStyles(theme => {
   return {
     root: {
       width: `calc(100% - 20px)`,
       margin: 10
+    },
+    media: {
+      height: 200
+    },
+    content: {
+      position: "relative"
+    },
+    info: {
+      [theme.breakpoints.up("sm")]: {
+        width: "calc(100% - 180px)",
+        display: "inline-block",
+        margin: 0,
+        clear: "none",
+        padding: "0 15px",
+        whiteSpace: "nowrap",
+        overflow: "hidden"
+      }
+    },
+    title: {
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis"
+    },
+    date: {
+      [theme.breakpoints.up("sm")]: {
+        position: "relative",
+        display: "inline-block",
+        width: 180,
+        borderRight: "1px #CCCCCC solid"
+      }
+    },
+    date1: {
+      [theme.breakpoints.down("xs")]: {
+        display: "inline-block"
+      }
+    },
+    date2: {
+      display: "inline-block",
+      clear: "none",
+      position: "relative",
+      margin: "0 7px",
+      [theme.breakpoints.up("sm")]: {
+        top: -5
+      }
+    },
+    date3: {
+      display: "inline-block",
+      clear: "none"
     }
   };
 });
@@ -18,18 +71,42 @@ export default function HackathonCard(props) {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Hackathon Name
-        </Typography>
-        <Typography variant="p" component="p">
-          This is a hackathon! Wow!
-        </Typography>
-      </CardContent>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={hackathonImg}
+          title="Hackathon"
+        />
+        <CardContent>
+          <div className={classes.date}>
+            <Typography className={classes.date1} variant="h6" component="p">
+              {props.startDate}
+            </Typography>
+            <Typography className={classes.date2} variant="body2" component="p">
+              TO
+            </Typography>
+            <Typography className={classes.date3} variant="h6" component="p">
+              {props.endDate}
+            </Typography>
+          </div>
+          <div className={classes.info}>
+            <Typography
+              className={classes.title}
+              variant="h4"
+              component="h2"
+              gutterBottom
+            >
+              {props.title}
+            </Typography>
+          </div>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
 
 HackathonCard.propTypes = {
-
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
